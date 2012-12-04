c4ecf723d455779325d1fd871a667306b52d4176
Florian Jenett <fjenett@gmail.com>
2012-12-04 08:46:28 +0100

    proper header comment for js lib

---
+++ res/js-header.js
@@ -0,0 +1,11 @@
+/**
+ *	PieceMaker API client (JavaScript version) for Motion Bank.
+ *
+ *	See: 
+ *	  http://piecemaker.org
+ *	  http://motionbank.org
+ *	  https://github.com/fjenett/piecemaker-api-client
+ *
+ *	Version: ##version##, build: ##build##
+ */
+ 
