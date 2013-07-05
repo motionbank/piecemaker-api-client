183fe23af573613001805e0a6537fdaeaee145a4
Florian Jenett <fjenett@gmail.com>
2013-07-05 09:16:52 +0200

    re-added missing dirs / files

---
+++ res/js-header.js
@@ -0,0 +1,14 @@
+/**
+ *	Piecemaker 2 API client (JavaScript version) for Motion Bank.
+ *
+ *	See: 
+ *	  https://github.com/motionbank/piecemaker2
+ *	  https://github.com/motionbank/piecemaker-api-client
+ *
+ *	Project:
+ *	  http://piecemaker.org
+ *	  http://motionbank.org
+ *
+ *	Version: ##version##, build: ##build##
+ */
+ 
