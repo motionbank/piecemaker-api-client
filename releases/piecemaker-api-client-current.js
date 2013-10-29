559d2a6d1a59503abf194b443a9f7db52f5a4a2c
Florian Jenett <fjenett@gmail.com>
2013-10-29 16:07:42 +0100

    starting 0.0.14

--- releases/piecemaker-api-client-current.js
+++ releases/piecemaker-api-client-current.js
@@ -9,7 +9,7 @@
  *	  http://piecemaker.org
  *	  http://motionbank.org
  *
- *	Version: 0.0.13, build: 764
+ *	Version: 0.0.14, build: 765
  */
  (function(){var s=function(i){var f=function(){},n=function(a){if(!a||"object"!==typeof a)return a;if("entrySet"in a&&"function"===typeof a.entrySet){var b=a.entrySet();if(!b)return a;a={};for(b=b.iterator();b.hasNext();){var c=b.next(),e=c.getValue();e&&("object"===typeof e&&"entrySet"in e&&"function"===typeof e.entrySet)&&(e=n(e));var d=c.getKey();if(!d)throw"Field key is not valid: "+d;a[c.getKey()]=e}return a}"utc_timestamp"in a&&(a.utc_timestamp=o(a.utc_timestamp));"created_at"in a&&(a.created_at=
 o(a.created_at));return a},l=function(a){if(a instanceof Array){for(var b=[],c=0;c<a.length;c++)b.push(k(h(a[c])));return b}return a},h=function(a){var b=a.event;b.fields={};for(var c=0,a=a.fields;c<a.length;c++)b.fields[a[c].id]=a[c].value;return b},k=function(a){a.fields.get=function(b){return a.fields[b]};a.utc_timestamp=new Date(1E3*a.utc_timestamp);return a},o=function(a){return a instanceof Date?a.getTime()/1E3:9999999999<a?a/1E3:a},m=function(a,b,c,e,u){if(!d.api_key&&!b.match(/\/user\/login$/))throw"PieceMakerApi: need an API_KEY, please login first to obtain one";
